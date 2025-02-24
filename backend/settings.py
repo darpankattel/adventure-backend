a430cce9233cf6ef8efeb2a485be78149fd7eb2f
darpankattel <kattelktl@gmail.com>
2025-02-25 00:53:09 +0545

    added static urls

--- backend/settings.py
+++ backend/settings.py
@@ -150,8 +150,8 @@ if not DEPLOYED:
     ]
     MEDIA_ROOT = BASE_DIR / 'media'
 else:
-    STATIC_ROOT = BASE_DIR / 'static'
-    MEDIA_ROOT = BASE_DIR / 'media'
+    STATIC_ROOT = "/opt/render/project/src/static"
+    MEDIA_ROOT = "/opt/render/project/src/media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
