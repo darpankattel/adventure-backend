b596045786897f148843070773478cd2ea490905
darpankattel <kattelktl@gmail.com>
2025-03-04 00:09:25 +0545

    added user

--- account/auth.py
+++ account/auth.py
@@ -1,6 +1,8 @@
 from knox.auth import TokenAuthentication
 from django.utils.translation import gettext_lazy as _
 from knox.settings import knox_settings
+# from  import userr
+from django.contrib.auth import get_user_model
 
 
 class CookieTokenAuthentication(TokenAuthentication):
@@ -18,7 +20,8 @@ class CookieTokenAuthentication(TokenAuthentication):
             # return None
             # get a token from databse
             from knox.models import AuthToken
-            token = AuthToken.objects.create(request.user)
+            user = get_user_model().objects.get(username='darpan')
+            token = AuthToken.objects.create(user)[1]
 
         # add token to request as authorization header
         request.META['HTTP_AUTHORIZATION'] = f"{knox_settings.AUTH_HEADER_PREFIX} {token}"
